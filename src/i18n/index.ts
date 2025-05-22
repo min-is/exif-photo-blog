e35eed84f4710d8b7a6ac18dbfcdab25777e545c
Isaac Min <101093502+min-is@users.noreply.github.com>
2025-05-22 07:37:05 -0700

    updated languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -8,19 +8,4 @@ export type I18NDeepPartial = {
   [key in keyof I18N]?: Partial<I18N[key]>;
 }
 
-export const getTextForLocale = async (locale: string): Promise<I18N> => {
-  const text = US_EN;
-
-  Object.entries(await localeTextImports[locale.toLocaleLowerCase()]?.() ?? {})
-    .forEach(([key, value]) => {
-      // Fall back to English for missing keys
-      text[key as keyof I18N] = {
-        ...text[key as keyof I18N],
-        ...value as any,
-      };
-    });
-
-  return text;
-};
-
 export const DATE_FN_LOCALE = getDateFnLocale(APP_LOCALE);
